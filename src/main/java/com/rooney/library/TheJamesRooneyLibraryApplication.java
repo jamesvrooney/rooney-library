4766f8c917664b4cca9cd9b61f47ea40ad5a4ce0
jamesvrooney <jamesvrooney@hotmail.com>
2021-06-23 17:20:25 +0100

    Initial commit

---
+++ src/main/java/com/rooney/library/TheJamesRooneyLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.rooney.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheJamesRooneyLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheJamesRooneyLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rooney/library/TheJamesRooneyLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rooney.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheJamesRooneyLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
